eb48659ef1954a3c4c65ef224a66f9c0fd059d35
Jordi Casanella <jordikiter@hotmail.es>
2023-12-08 00:16:41 +0000

    First version dialog modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,8 +104,23 @@ const signInFnc = async () => {
             const resp = await response.json();
             console.log(`Resp: ${resp.token}`);
         } catch(err) {
+            var modal = document.getElementById("modalDialog");
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+
             if (err instanceof HttpError) {
-                alert(`User invalid!!!`);
+                modal.style.display = "block";
             }
         }
     }
